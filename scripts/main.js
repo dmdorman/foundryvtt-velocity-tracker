65d6673520b8174cae9b703d24b81797eb318012
dmdorman <drewdorguy@gmail.com>
2023-01-12 01:20:31 -0600

    starting to read in data values

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,10 @@ Hooks.on("ready", function() {
 class VelocityTracker extends FormApplication {
     constructor() {
         super();
+        this.data = {
+            name: "character/vehicle name",
+            maxMove: 0
+        };
     }
 
     static get defaultOptions() {
@@ -34,10 +38,58 @@ class VelocityTracker extends FormApplication {
     }
 
     getData() {
-        super.getData();
+        const data = super.getData()
+    
+        data.name = "character/vehicle name"
+        data.maxMove = 0
+        data.maxAction = 0
+        data.maxVel = 0
+        //data = this.data;
+
+        //data.name = this.data.name;
+        //data.other = "Its me I'm other"
+    
+        return data
     }
 
     activateListeners(html) {
         super.activateListeners(html);
+
+        html.find('input').each((id, inp) => {
+            this.changeValue = async function (e) {
+              if (e.code === 'Enter' || e.code === 'Tab') {
+                let key = e.target.dataset.key
+
+                if (e.target.dataset.dtype === 'Number') {
+                  this.data.name = e.target.value
+
+                  if (isNaN(parseInt(e.target.value))) {
+                    console.log(e.target.dataset.key)
+
+                    this.data[`${key}`] = "bruh"
+
+                    console.log(this.data)
+
+                    //return
+                  }
+      
+                  console.log(e.target.value)
+                  //const changes = []
+                  //changes[`system.characteristics.${e.target.name}`] = parseInt(e.target.value)
+                  //await this.actor.update(changes)
+                  
+                } else {
+                  //this._updateName(e.target.value)
+                  console.log('else!')
+                  console.log(e.target.value)
+                }
+
+                //this.getData()
+                //this.render()
+              }
+            }
+
+            inp.addEventListener('keydown', this.changeValue.bind(this))
+        })
     }
 }
