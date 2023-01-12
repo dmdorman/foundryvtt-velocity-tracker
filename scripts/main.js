bb5ac51cf873a201ef22b9b2d402e51c49c2f05f
dmdorman <drewdorguy@gmail.com>
2023-01-11 21:29:56 -0600

    added scene button

--- scripts/main.js
+++ scripts/main.js
@@ -2,36 +2,42 @@ console.log("Hello World! This code runs immediately when the file is loaded.");
 
 Hooks.on("init", function() {
     console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
+
+    Hooks.on("getSceneControlButtons", (controls) => {
+        controls[0].tools.push({
+        name: 'velocity tracker',
+        title: 'Velocity Tracker',
+        icon: 'far fa-up',
+        button: true,
+        onClick: () => new VelocityTracker().render(true),
+        visible: true
+        })
+    });
 });
 
 Hooks.on("ready", function() {
     console.log("This code runs once core initialization is ready and game data is available.");
-
-    async function createPopOut() {
-        //const content = await _renderForm();
-        const templateData = {};
-
-        var path = "modules/velocity-tracker/templates/tracker.hbs";
-        1
-        const content = await renderTemplate(path, templateData);
-
-        // Attack Card as a Pop Out
-        let options = {
-            'width' : 300,
-        }
-
-        //return new Promise(resolve => {
-            const data = {
-                title: "Velocity Tracker",
-                content: content,
-                buttons: {},
-                default: "none",
-                //close: () => resolve({})
-            }
-
-            new Dialog(data, options).render(true);;
-        //});
+});
+class VelocityTracker extends FormApplication {
+    constructor() {
+        super();
     }
 
-    createPopOut()
-});
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ['velocity-tracker-gui'],
+            popOut: true,
+            template: './modules/velocity-tracker/templates/tracker.hbs',
+            id: 'velocity-tracker-application',
+            title: 'Velocity Tracker',
+        });
+    }
+
+    getData() {
+        super.getData();
+    }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+    }
+}
